d7b1390158d1307fa3177d9a048d6d8788dbeb8f
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-04-29 18:58:35 -0400

    allow the overrriding of the yaml converter

--- automation-engine-http/src/test/java/com/automation/engine/http/YamlConverterWith100MBLimit.java
+++ automation-engine-http/src/test/java/com/automation/engine/http/YamlConverterWith100MBLimit.java
@@ -1,15 +1,15 @@
-package com.automation.engine.converter;
+package com.automation.engine.http;
 
-import com.automation.engine.conditional.AEConditionalOnMissingBean;
 import com.automation.engine.creator.parsers.yaml.IYamlConverter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.LoaderOptions;
 
 @Service
-@AEConditionalOnMissingBean(IYamlConverter.class)
-public class YamlConverter implements IYamlConverter {
+public class YamlConverterWith100MBLimit implements IYamlConverter {
     @Override
     public <T> T convert(String yaml, Class<T> clazz) {
         try {
@@ -21,8 +21,14 @@ public class YamlConverter implements IYamlConverter {
     }
 
     public ObjectMapper getYamlObjectMapper() {
+        var options = new LoaderOptions();
+        options.setCodePointLimit(100 * 1024 * 1024);
+        var factory = YAMLFactory.builder()
+                .loaderOptions(options)
+                .build();
         return Jackson2ObjectMapperBuilder
                 .yaml()
+                .factory(factory)
                 .build();
     }
 
