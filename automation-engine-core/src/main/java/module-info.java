8fa6a4c555b76f7470198903b771cae76a4ad302
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-05-09 20:02:44 -0400

    removing the maven compiler as it's causing isseus

--- automation-engine-core/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.davidrandoll.automation.engine {
-    requires com.fasterxml.jackson.annotation;
-    requires jakarta.validation;
-    requires static lombok;
-    requires org.slf4j;
-    requires org.apache.commons.lang3;
-}
