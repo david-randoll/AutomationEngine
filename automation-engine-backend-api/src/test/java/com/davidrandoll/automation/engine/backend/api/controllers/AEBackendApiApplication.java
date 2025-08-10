3d4fb1df4d2fbe4f856abf14b7912b0a7122b056
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-08-10 12:46:49 -0400

    Add test config and initial Next.js UI app
    Added test dependencies and a test security configuration to the backend API. Renamed the test application class for clarity. Introduced a new Next.js UI app with initial configuration, README, and supporting files.

--- automation-engine-backend-api/src/test/java/com/davidrandoll/automation/engine/backend/api/controllers/AEBackendApiApplication.java
+++ automation-engine-backend-api/src/test/java/com/davidrandoll/automation/engine/backend/api/controllers/AEBackendApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutomationEngineApplication {
+public class AEBackendApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AutomationEngineApplication.class, args);
+        SpringApplication.run(AEBackendApiApplication.class, args);
     }
 }
