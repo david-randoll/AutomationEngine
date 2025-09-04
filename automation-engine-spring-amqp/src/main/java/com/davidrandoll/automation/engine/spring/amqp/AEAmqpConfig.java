8e6de481e158b8971b38da67dce35aefba80e059
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-09-04 19:39:02 -0400

    Update AMQP dependencies and config annotations
    Replaces spring-boot-starter-amqp with spring-amqp in automation-engine-spring-amqp module and adds ConditionalOnClass annotation to AEAmqpConfig. Adds spring-boot-starter-amqp dependency to todo-example for AMQP support.

--- automation-engine-spring-amqp/src/main/java/com/davidrandoll/automation/engine/spring/amqp/AEAmqpConfig.java
+++ automation-engine-spring-amqp/src/main/java/com/davidrandoll/automation/engine/spring/amqp/AEAmqpConfig.java
@@ -3,11 +3,13 @@ package com.davidrandoll.automation.engine.spring.amqp;
 import com.davidrandoll.automation.engine.spring.amqp.actions.PublishRabbitMqEventAction;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass({AmqpTemplate.class, AmqpAdmin.class})
 public class AEAmqpConfig {
     @Bean("publishRabbitMqEventAction")
     @ConditionalOnMissingBean(name = "publishRabbitMqEventAction", ignored = PublishRabbitMqEventAction.class)
