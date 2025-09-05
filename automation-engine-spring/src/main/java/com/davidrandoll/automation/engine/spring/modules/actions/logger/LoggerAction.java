4a27797391daff13d08a9b057210a49a2587aac6
d-randoll <51640895+d-randoll@users.noreply.github.com>
2025-09-05 19:12:58 -0400

    Add example contexts to LoggerAction
    Introduced the getExamples() method in LoggerAction to provide sample LoggerActionContext instances for info and debug log messages. This helps illustrate usage patterns and can assist with documentation or testing.

--- automation-engine-spring/src/main/java/com/davidrandoll/automation/engine/spring/modules/actions/logger/LoggerAction.java
+++ automation-engine-spring/src/main/java/com/davidrandoll/automation/engine/spring/modules/actions/logger/LoggerAction.java
@@ -4,6 +4,9 @@ package com.davidrandoll.automation.engine.spring.modules.actions.logger;
 import com.davidrandoll.automation.engine.core.events.EventContext;
 import com.davidrandoll.automation.engine.spring.spi.PluggableAction;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.event.Level;
+
+import java.util.List;
 
 @Slf4j
 public class LoggerAction extends PluggableAction<LoggerActionContext> {
@@ -11,5 +14,23 @@ public class LoggerAction extends PluggableAction<LoggerActionContext> {
     @Override
     public void doExecute(EventContext ec, LoggerActionContext ac) {
         log.atLevel(ac.getLevel()).log("{}", ac.getMessage());
+    }
+
+    @Override
+    public List<LoggerActionContext> getExamples() {
+        return List.of(
+                new LoggerActionContext(
+                        "simple-info",
+                        "Log a simple info message",
+                        Level.INFO,
+                        "Automation started successfully"
+                ),
+                new LoggerActionContext(
+                        "debug-variables",
+                        "Log a debug message with variable placeholders",
+                        Level.DEBUG,
+                        "Received request with id={{ event.id }} and payload={{ event.body }}"
+                )
+        );
     }
 }
