ee977861761cd1336577dfbc0ba358e03c3327f2
David Randoll <51640895+david-randoll@users.noreply.github.com>
2025-12-24 19:37:00 -0500

    Add ObjectMapper to TemplateProcessor for variable conversion
    TemplateProcessor now accepts an ObjectMapper to convert variables, ensuring JsonNode values are handled before Pebble template evaluation. Updated AETemplatingConfig to inject ObjectMapper into TemplateProcessor bean.

--- automation-engine-templating/src/main/java/com/davidrandoll/automation/engine/templating/TemplateProcessor.java
+++ automation-engine-templating/src/main/java/com/davidrandoll/automation/engine/templating/TemplateProcessor.java
@@ -1,6 +1,8 @@
 package com.davidrandoll.automation.engine.templating;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.pebbletemplates.pebble.PebbleEngine;
 import io.pebbletemplates.pebble.template.PebbleTemplate;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class TemplateProcessor {
     private final PebbleEngine pebbleEngine;
+    private final ObjectMapper mapper;
 
     /**
      * Processes a template string with the provided variables.
@@ -29,10 +32,14 @@ public class TemplateProcessor {
      * @throws IOException If there is an error during template processing.
      */
     public String process(String templateString, Map<String, Object> variables) throws IOException {
+        // need to ensure that variables don't have any JsonNode values. because pebble can't handle them directly
+        Map<String, Object> processedVariables = mapper.convertValue(variables, new TypeReference<>() {
+        });
+
         PebbleTemplate template = pebbleEngine.getLiteralTemplate(templateString);
 
         try (StringWriter writer = new StringWriter()) {
-            template.evaluate(writer, variables);
+            template.evaluate(writer, processedVariables);
             return writer.toString();
         }
     }
